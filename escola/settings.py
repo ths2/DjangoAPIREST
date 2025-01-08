55af99498315b8f903cd0fd320cff1384619a99c
Thomas Silva <ths2.thomas.com>
2025-01-08 14:50:16 -0300

    Autenticação via Token

--- escola/settings.py
+++ escola/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cursos',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken' #Auth via token
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # DRF
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
 }
+
+
+#'43b183fb48c0a8fc510743a63fe50eacdf0549dc'

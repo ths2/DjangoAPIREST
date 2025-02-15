cdc4304d2ff09312909d68b15fe7e01652d108a4
Thomas Silva <ths2.thomas.com>
2025-02-15 17:19:25 -0300

    Validações

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Curso, Avaliacao
+from django.db.models import Avg
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
 
@@ -18,6 +19,11 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'criacao',
             'ativo'
         )
+    
+    def validate_avaliacao(self, valor):
+        if valor in range(1,6):
+            return valor
+        raise serializers.ValidationError('A nota da avaliação precisa ser entre 1 e 5')
 
 class CursoSerizalizer(serializers.ModelSerializer):
 
@@ -30,6 +36,8 @@ class CursoSerizalizer(serializers.ModelSerializer):
     # 3.Primary Key Related Field
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
         fields = (
@@ -38,5 +46,12 @@ class CursoSerizalizer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+    
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        if media is None:
+            return 0
+        return round(media*2)/2
